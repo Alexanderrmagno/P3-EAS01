a2fed120a21cb17d0795788085005fe28009364e
Alexandrrmagno <22192@portalcedi.edu.mx>
2024-04-29 12:55:34 -0600

    somalia

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     // setup
     RenderWindow window(VideoMode(800, 600), "SFML works!");
     window.setFramerateLimit(60);
-    Rectangle rect(Vector2f(30,30));
+    Rectangle rect(Vector2f(30, 30));
+    vector<Rectangle> rectangle;
 
     // loop
     while (window.isOpen())
@@ -24,14 +25,29 @@ int main()
             /** (5pts)
                 Cuando se de click izquierdo, se deberá actualizar el objetivo del cuadrado a la posición del mouse.
             */
+            if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    int w = 50;
+                    int h = 50;
+                    int x = event.mouseButton.x;
+                    int y = event.mouseButton.y;
+                    Rectangle rec(Vector2f(w, h), Vector2i(x, y));
+                    rectangle.push_back(rec);
+                }
+            }
 
+            window.clear();
+            rect.update();
+            rect.drawTo(window);
+            for (auto &r : rectangle)
+            {
+                r.update();
+                r.drawTo(window);
+            }
+            window.display();
         }
 
-        window.clear();
-        rect.update();
-        rect.drawTo(window);
-        window.display();
+        return 0;
     }
-
-    return 0;
-}
